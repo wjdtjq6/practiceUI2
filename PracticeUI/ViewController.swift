52d47aaf7f8993f196d9439d503f005036755497
t2023-m0032 <t2023-m0032@t2023-m0032ui-MacBookAir.local>
2024-03-01 17:36:46 +0900

    first commit

---
+++ PracticeUI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PracticeUI
+//
+//  Created by t2023-m0032 on 2/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let friendNames: [String] = ["jimmy", "kimchi","hi"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
